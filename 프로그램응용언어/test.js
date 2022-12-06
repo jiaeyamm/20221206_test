634878e61e3986d0408659291618671c57150c9c
jiaeyamm <mjiae1125@gmail.com>
2022-12-06 21:26:07 +0900

    20221206 test
    배열, 이벤트 시험

---
+++ 프로그램응용언어/test.js
@@ -0,0 +1,17 @@
+// userList 안의 객체들 중 age propety 값이 30인 이상인 객체의 이름만 모아서 result 배열에 담기
+// 배열의 고차함수들을 이용하세용
+// filtering과 데이터 변형
+let userlist = [
+  { name: "Minsu", age: 20 },
+  { name: "seok", age: 30 },
+  { name: "Yeji", age: 23 },
+  { name: "Minseok", age: 43 },
+];
+
+userlist.filter(function () {
+  return userlist.forEach[1] >= 30;
+});
+let result = userlist;
+console.log(result);
+
+// map을 써야되는건 알겠는데 모르겠어요.............
